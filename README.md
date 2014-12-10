0dceec351c4108b4f7f6f9dbe242482403e7b109
Fabian Schmengler <fschmengler@sgh-it.eu>
2014-12-11 00:20:31 +0100

    Initial fish  ¸.·´¯`·.´¯`·.¸¸.·´¯`·.¸><(((º>

---
+++ README.md
@@ -0,0 +1,14 @@
+SSE_AclReload
+======
+Adds a menu entry "Reload ACL" to Magento admin panel. Useful after installing extensions instead of logging out and back in.
+
+The link also appears directly on admin 404 pages:
+
+![Screenshot](https://github.com/schmengler/AclReload/raw/master/screenshot-404.png)
+
+
+## Version 
+* Version 0.1.0
+
+## License 
+* see [LICENSE](https://github.com/schmengler/AclReload/blob/master/license.txt) file
---
+++ src/app/code/community/SSE/AclReload/controllers/Adminhtml/Permissions/AclReloadController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of SSE_AclReload for Magento.
+ *
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @author Fabian Schmengler <fabian@schmengler-se.de>
+ * @category SSE
+ * @package SSE_AclReload
+ * @copyright Copyright (c) 2014 Schmengler Software Engineering (http://www.schmengler-se.de/)
+ */
+
+class SSE_AclReload_Adminhtml_Permissions_AclReloadController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $session = Mage::getSingleton('admin/session');
+        $session->setAcl(Mage::getResourceModel('admin/acl')->loadAcl());
+
+        Mage::getSingleton('adminhtml/session')->addSuccess($this->__('ACL reloaded'));
+        $this->_redirectReferer();
+    }
+}
