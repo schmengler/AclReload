27f3397ede204c88464edd0d753e2c8b31b46cd1
Fabian Schmengler <fs@integer-net.de>
2015-01-26 14:03:46 +0100

    Translations and Helper definition (fixed #1)

---
+++ src/app/code/community/SSE/AclReload/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of SSE_AclReload for Magento.
+ *
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @author Fabian Schmengler <fabian@schmengler-se.de>
+ * @category SSE
+ * @package SSE_AclReload
+ * @copyright Copyright (c) 2014 Schmengler Software Engineering (http://www.schmengler-se.de/)
+ */
+
+/**
+ * Default helper
+ */
+class SSE_AclReload_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
